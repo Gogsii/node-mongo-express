cec1271ae0f21b9960fce05b586ea5412798185d
gogsi <dragutinovic.g@gmail.com>
2021-01-29 10:35:29 -0800

    "Converted Common JS module to ES6 import"

--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,29 @@
-exports.perimeter = (x, y) => 2 * (x + y);
-exports.area = (x, y) => x * y;
+//ECMA6
+export default (x, y, callback) => {
+    if (x <= 0 || y <= 0) {
+        callback(new Error('Rectangle dimensions must be greater than zero'))
+    } else {
+        setTimeout(() =>
+            callback(null, {
+                perimeter: (x, y) => 2 * (x + y),
+                area: (x, y) => x * y
+            }),
+            2000
+        )
+    }
+}
+
+// COMMON JS
+// module.exports = (x, y, callback) => {
+//     if (x <= 0 || y <= 0) {
+//         callback(new Error('Rectangle dimensions must be greater than zero'))
+//     } else {
+//         setTimeout(() =>
+//             callback(null, {
+//                 perimeter: (x, y) => 2 * (x + y),
+//                 area: (x, y) => x * y
+//             }),
+//             2000
+//         )
+//     }
+// }
---
+++ rectangle2.mjs
@@ -0,0 +1,13 @@
+export default (x, y, callback) => {
+    if (x <= 0 || y <= 0) {
+        callback(new Error('Rectangle dimensions must be greater than zero'))
+    } else {
+        setTimeout(() =>
+            callback(null, {
+                perimeter: (x, y) => 2 * (x + y),
+                area: (x, y) => x * y
+            }),
+            2000
+        )
+    }
+}
